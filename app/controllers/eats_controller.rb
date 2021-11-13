466e04c20e1a7bf2dca1ce64fa8a6ec9f7abd900
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-11-13 07:26:07 +0000

    create

--- app/controllers/eats_controller.rb
+++ app/controllers/eats_controller.rb
@@ -1,24 +1,42 @@
 class EatsController < ApplicationController
   
   def new
+    @food = Food.new
   end
   
   def create
+    @food = Food.new
+    @food.save(food_params)
+    redirect_to eats_path(@food.id)
   end
   
   def index
+    @foods = Food.all
   end 
   
   def show
+    @food = Food.find(params[:id])
   end
   
   def edit
+    @food = Food.find(params[:id])
   end
   
   def update
+    @food = Food.find(params[:id])
+    @food.update(food_params)
+    redirect_to eats_path
   end
   
   def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to eats_path
+  end
+  
+  private
+  def food_params
+    params.require(:food).permit(:name,:body)
   end
   
   
